38a887eb18424702ac5a28c3f48581a046367ebc
Asher <momomitchellsmith@gmail.com>
2024-05-01 14:07:40 -0700

    add client boilerplate, render App.jsx

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    //render a container
+
+    <div id='app' className='container'>
+      <h1>test heading :)</h1>
+    </div>
+  );
+};
+
+export default App;
