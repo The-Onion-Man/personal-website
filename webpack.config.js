15a3d0cfe6e733ef4e83f864e7641dcc3a661d55
Asher <momomitchellsmith@gmail.com>
2024-05-03 12:10:31 -0700

    add stylesheet as an entry in webpack, solving issue with it not being loaded on 8080

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,67 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
+  entry: ['./src/index.js', './src/styles.css'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
 
-    module: {
-        rules: [
-            {
-                //JS and JSX
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
+  module: {
+    rules: [
+      {
+        //JS and JSX
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
 
-            // For .css
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+      // For .css
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // Images, GIFs, and vector graphics
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            // loader: 'url-loader',
+            loader: 'file-loader',
+            options: {
+              limit: 8192, // 8kb. Files smaller than this get inlined as base64
+              fallback: 'file-loader', // Use file-loader as a fallback for larger images
+              name: 'images/[name].[hash:8].[ext]', // The output path for the images when file-loader is used
             },
-            {
-                // Images, GIFs, and vector graphics
-                test: /\.(png|jpg|gif|svg)$/,
-                use: [
-                    {
-                        // loader: 'url-loader',
-                        loader: 'file-loader',
-                        options: {
-                            limit: 8192, // 8kb. Files smaller than this get inlined as base64
-                            fallback: 'file-loader', // Use file-loader as a fallback for larger images
-                            name: 'images/[name].[hash:8].[ext]' // The output path for the images when file-loader is used
-                        }
-                    }
-                ]
-            },
-        ]
-    },
-    plugins: [
-        //plugin to creat html files to serve bundles
-        new htmlWebpackPlugin({
-            template: './src/index.html'
-        })
+          },
+        ],
+      },
     ],
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 8080,
-        proxy: {
-            '**': 'http://localhost:3000',
-        }
+  },
+  plugins: [
+    //plugin to creat html files to serve bundles
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    proxy: {
+      '**': 'http://localhost:3000',
     },
-    resolve: {
-        extensions: ['.*', '.js', '.jsx']
-    },
-}
-
-
-
-
-
-
+  },
+  resolve: {
+    extensions: ['.*', '.js', '.jsx'],
+  },
+};
